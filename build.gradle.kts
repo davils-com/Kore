82b2a23da97e9409dcca0b7da35369a03c0425d4
Nils Jaekel <nils.jaekel@davils.com>
2025-07-01 13:12:14 +0200

    feat: add dependencies for coroutines, serialization, datetime, logging, and testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,26 @@
 plugins {
     `kore-core`
 }
+
+group = "com.davils.kore"
+
+dependencies {
+    // coroutines
+    api(libs.kotlinx.coroutines.core)
+    api(libs.kotlinx.coroutines.reactive)
+
+    // serialization
+    api(libs.kotlinx.serialization.json)
+    api(libs.kaml)
+
+    // datetime
+    api(libs.kotlinx.datetime)
+
+    // logging
+    api(libs.logback)
+
+    // Testing
+    testImplementation(libs.kotest.assertion.core)
+    testImplementation(libs.kotest.property)
+    testImplementation(libs.kotest.runner.junit5)
+}
