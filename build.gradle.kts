427148631e5fcd33d4a9c530c744a62c227421bd
Nils Jaekel <nils.jaekel@davils.com>
2025-07-01 12:48:45 +0200

    feat: refactor build configuration and add kore-core plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,3 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
-}
-
-group = "com.davils.kore"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    `kore-core`
 }
---
+++ buildSrc/src/main/kotlin/kore-core.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    com.davils.kreate
+}
+
+kreate {
+    core {
+        description = "Kore Core"
+        javaVersion = JavaVersion.VERSION_21
+        explicitApi = true
+        allWarningsAsErrors = true
+    }
+}
